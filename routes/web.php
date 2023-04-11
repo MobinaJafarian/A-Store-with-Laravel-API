6d077665e9d84e9505db7a33c481ab873c85737d
mobinajafarian <nimobina99@gmail.com>
2023-04-11 16:32:07 +0330

    update admin route

--- routes/web.php
+++ routes/web.php
@@ -15,7 +15,7 @@ use Illuminate\Support\Facades\Route;
 */
 
 Route::get('/', function () {
-    return view('welcome');
+    return redirect()->route('login');
 });
 
 Route::get('/dashboard', function () {
@@ -27,5 +27,13 @@ Route::middleware('auth')->group(function () {
     Route::patch('/profile', [ProfileController::class, 'update'])->name('profile.update');
     Route::delete('/profile', [ProfileController::class, 'destroy'])->name('profile.destroy');
 });
+
+
+Route::middleware('auth')->group(function () {
+    Route::get('/admin',function(){
+        return view('admin.index');
+    });
+});
+
 
 require __DIR__.'/auth.php';
