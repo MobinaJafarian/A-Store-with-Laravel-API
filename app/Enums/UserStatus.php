545170d34ec2e6262865b236e5942e1a9500ae73
mobinajafarian <nimobina99@gmail.com>
2023-04-11 15:34:15 +0330

    initial commit

---
+++ app/Enums/UserStatus.php
@@ -0,0 +1,9 @@
+<?php
+
+namespace App\Enums;
+
+enum UserStatus:string{
+    case Active = 'active';
+
+    case InActive = 'inactive';
+}
---
+++ database/migrations/2014_10_12_000000_create_users_table.php
@@ -0,0 +1,39 @@
+<?php
+
+use App\Enums\UserStatus;
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::create('users', function (Blueprint $table) {
+            $table->id();
+            $table->string('name')->nullable();
+            $table->string('user_name' , 50)->nullable();
+            $table->string('mobile')->unique()->nullable();
+            $table->string('email')->unique()->nullable();
+            $table->timestamp('email_verified_at')->nullable();
+            $table->string('phone')->unique()->nullable();
+            $table->string('photo')->nullable();
+            $table->boolean('is_admin')->default(false);
+            $table->string('status')->default(UserStatus::Active->value);
+            $table->string('password');
+            $table->rememberToken();
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::dropIfExists('users');
+    }
+};
