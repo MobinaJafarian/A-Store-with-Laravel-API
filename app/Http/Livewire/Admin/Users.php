ccc497ffa0f79a1f23b640622e8f243b38463147
mobinajafarian <nimobina99@gmail.com>
2023-04-14 16:54:59 +0330

    add changeUserStatus

--- app/Http/Livewire/Admin/Users.php
+++ app/Http/Livewire/Admin/Users.php
@@ -2,12 +2,31 @@
 
 namespace App\Http\Livewire\Admin;
 
+use App\Enums\UserStatus;
 use App\Models\User;
 use Livewire\Component;
+use Livewire\WithPagination;
 
 class Users extends Component
 {
+    use WithPagination;
+
+    protected $paginationTheme='bootstrap';    
     public $search;
+
+    public function changeUserStatus($id)
+    {
+        $user = User::query()->find($id);
+        if ($user->status == UserStatus::Active->value) {
+            $user->update([
+                'status' => UserStatus::InActive->value
+            ]);
+        }else {
+            $user->update([
+                'status' => UserStatus::Active->value
+            ]);
+        }
+    }
 
     public function render()
     {
