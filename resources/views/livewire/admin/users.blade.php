d5ae489e8e363d8c28c8843f804711e5d40c593b
mobinajafarian <nimobina99@gmail.com>
2023-04-15 14:36:58 +0330

    update route name

--- resources/views/livewire/admin/users.blade.php
+++ resources/views/livewire/admin/users.blade.php
@@ -47,7 +47,7 @@
 
                 </td>
                 <td class="text-center align-middle">
-                    <a class="btn btn-outline-info" href="#">
+                    <a class="btn btn-outline-info" href="{{ route('users.edit', $user->id)}}">
                         ویرایش
                     </a>
                 </td>
