c80589c9176103b0f40e87bed8ae437aceddfcd4
mobinajafarian <nimobina99@gmail.com>
2023-04-15 14:35:56 +0330

    update route name

--- resources/views/admin/layouts/navigation.blade.php
+++ resources/views/admin/layouts/navigation.blade.php
@@ -25,8 +25,8 @@
             <li>
                 <a href="#">کاربران</a>
                 <ul>
-                    <li><a href="{{ route('user.create') }}">ایجاد کاربر</a></li>
-                    <li><a href="{{ route('user.index') }}">لیست کاربران</a></li>
+                    <li><a href="{{ route('users.create') }}">ایجاد کاربر</a></li>
+                    <li><a href="{{ route('users.index') }}">لیست کاربران</a></li>
                 </ul>
             </li>
         </ul>
