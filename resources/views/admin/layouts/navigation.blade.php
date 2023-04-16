d90e98b5d6f2f5337c85a37f4811cc988b3a1d36
mobinajafarian <nimobina99@gmail.com>
2023-04-16 15:12:47 +0330

    update navigation

--- resources/views/admin/layouts/navigation.blade.php
+++ resources/views/admin/layouts/navigation.blade.php
@@ -1,12 +1,23 @@
 <div class="navigation">
     <div class="navigation-icon-menu">
+
         <ul>
+{{--            @hasanyrole('')--}}
             <li data-toggle="tooltip" title="کاربران">
                 <a href="#users" title=" کاربران">
                     <i class="icon ti-user"></i>
                 </a>
             </li>
+{{--            @endhasanyrole--}}
+
+            <li data-toggle="tooltip" title="محصولات">
+                <a href="#products" title=" محصولات">
+                    <i class="icon ti-folder"></i>
+                </a>
+            </li>
+
         </ul>
+
         <ul>
             <li data-toggle="tooltip" title="ویرایش پروفایل">
                 <a href="#" class="go-to-page">
@@ -14,9 +25,13 @@
                 </a>
             </li>
             <li data-toggle="tooltip" title="خروج">
-                <a href="login.html" class="go-to-page">
-                    <i class="icon ti-power-off"></i>
-                </a>
+                <form name="myform" action="{{route('logout')}}" method="post">
+                    @csrf
+                    <input type='hidden' name='query'/>
+                    <a href="javascript: submitform()" class="go-to-page">
+                        <i class="icon ti-power-off"></i>
+                    </a>
+                </form>
             </li>
         </ul>
     </div>
@@ -25,27 +40,76 @@
             <li>
                 <a href="#">کاربران</a>
                 <ul>
-                    <li><a href="{{ route('users.create') }}">ایجاد کاربر</a></li>
-                    <li><a href="{{ route('users.index') }}">لیست کاربران</a></li>
+                    <li><a href="{{route('users.create')}}">ایجاد کاربر</a></li>
+                    <li><a href="{{route('users.index')}}">لیست کاربران</a></li>
                 </ul>
             </li>
-            
             <li>
-                <a href="#">نقش ها </a>
+                <a href="#">نقش ها</a>
                 <ul>
-                    <li><a href="{{ route('roles.create') }}">ایجاد نقش</a></li>
-                    <li><a href="{{ route('roles.index') }}">لیست نقش ها</a></li>
+                    <li><a href="{{route('roles.create')}}">ایجاد نقش</a></li>
+                    <li><a href="{{route('roles.index')}}">لیست نقش ها</a></li>
                 </ul>
             </li>
-            
             <li>
                 <a href="#">لاگ ها</a>
                 <ul>
-                    <li><a href="{{ route('logs') }}">لیست لاگ ها</a></li>
+                    <li><a href="{{route('logs')}}">لیست لاگ ها</a></li>
+
                 </ul>
             </li>
-
+        </ul>
+        <ul id="products">
+            <li>
+                <a href="#">دسته بندی</a>
+                <ul>
+                    <li><a href="{{route('category.create')}}">ایجاد دسته بندی</a></li>
+                    <li><a href="{{route('category.index')}}">لیست دسته بندی ها</a></li>
+                </ul>
+            </li>
+            <li>
+                <a href="#"> اسلایدر</a>
+                <ul>
+                    {{-- <li><a href="{{route('sliders.create')}}">ایجاد اسلایدر</a></li> --}}
+                    {{-- <li><a href="{{route('sliders.index')}}">لیست اسلایدر</a></li> --}}
+                </ul>
+            </li>
+            <li>
+                <a href="#"> برندها</a>
+                <ul>
+                    {{-- <li><a href="{{route('brands.create')}}">ایجاد برند</a></li> --}}
+                    {{-- <li><a href="{{route('brands.index')}}">لیست برندها</a></li> --}}
+                </ul>
+            </li>
+            <li>
+                <a href="#"> رنگ ها</a>
+                <ul>
+                    {{-- <li><a href="{{route('colors.create')}}">ایجاد رنگ</a></li>
+                    <li><a href="{{route('colors.index')}}">لیست رنگ ها</a></li> --}}
+                </ul>
+            </li>
+            <li>
+                <a href="#"> محصولات</a>
+                <ul>
+                    {{-- <li><a href="{{route('products.create')}}">ایجاد محصول</a></li> --}}
+                    {{-- <li><a href="{{route('products.index')}}">لیست محصولات</a></li> --}}
+                </ul>
+            </li>
+            <li>
+                <a href="#"> گروه ویژگی ها</a>
+                <ul>
+                    {{-- <li><a href="{{route('property_groups.create')}}">ایجاد گروه ویژگی ها</a></li> --}}
+                    {{-- <li><a href="{{route('property_groups.index')}}">لیست گروه ویژگی ها</a></li> --}}
+                </ul>
+            </li>
+            <li>
+                <a href="#"> ویژگی ها</a>
+                <ul>
+                    {{-- <li><a href="{{route('properties.create')}}">ایجاد ویژگی </a></li> --}}
+                    {{-- <li><a href="{{route('properties.index')}}">لیست  ویژگی ها</a></li> --}}
+                </ul>
+            </li>
         </ul>
     </div>
 </div>
-<!-- end::navigation -->
+Footer
