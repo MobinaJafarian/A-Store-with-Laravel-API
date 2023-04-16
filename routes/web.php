0ed4ae0f476ae207ff089ae2e7ff3e342352c798
mobinajafarian <nimobina99@gmail.com>
2023-04-16 15:11:28 +0330

    add category route

--- routes/web.php
+++ routes/web.php
@@ -1,5 +1,6 @@
 <?php
 
+use App\Http\Controllers\Admin\CategoryController;
 use App\Http\Controllers\Admin\LogViewerController;
 use App\Http\Controllers\Admin\PanelController;
 use App\Http\Controllers\Admin\RoleController;
@@ -34,12 +35,20 @@ Route::middleware('auth')->group(function () {
 
 
 Route::prefix('admin')->middleware('auth')->group(function () {
+    
+    //Main Route
     Route::get('/', [PanelController::class , 'index'])->name('panel');
+    
+    //User Route
     Route::resource('/users', UserController::class);
     Route::resource('/roles', RoleController::class);
     Route::get('create_user_roles/{id}', [UserController::class , 'createUserRoles'])->name('create.user.roles');
     Route::post('store_user_roles/{id}', [UserController::class , 'storeUserRoles'])->name('store.user.roles');
     Route::get('logs', [LogViewerController::class , 'index'])->name('logs');
+
+    //Product Route
+    Route::resource('category', CategoryController::class);
+
 });
 
 
