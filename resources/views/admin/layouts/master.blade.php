cb0b744d7d7f42a9cdc6155be32dcd853be6cb7d
mobinajafarian <nimobina99@gmail.com>
2023-04-18 01:23:16 +0330

    add sweet_alert

--- resources/views/admin/layouts/master.blade.php
+++ resources/views/admin/layouts/master.blade.php
@@ -13,6 +13,7 @@
 	<link rel="stylesheet" href="{{asset('panel/vendors/vmap/jqvmap.min.css')}}">
 	<link rel="stylesheet" href="{{asset('panel/assets/css/app.css')}}" type="text/css">
 	<link rel="stylesheet" href="{{asset('panel/vendors/select2/css/select2.min.css')}}">
+	<link rel="stylesheet" href="{{url('panel/plugins/sweet_alert/sweetalert2.min.css')}}" type="text/css">
 </head>
 <body class="small-navigation">
 	
@@ -28,6 +29,7 @@
 	@livewireScripts
 	<script src="{{asset('panel/vendors/bundle.js')}}"></script>
 	<script src="{{url('panel/vendors/select2/js/select2.min.js')}}"></script>
+	<script src="{{url('panel/plugins/sweet_alert/sweetalert2.all.min.js')}}"></script>
 	<script src="{{asset('panel/vendors/slick/slick.min.js')}}"></script>
 	<script src="{{asset('panel/vendors/vmap/jquery.vmap.min.js')}}"></script>
 	<script src="{{asset('panel/assets/js/app.js')}}"></script>
