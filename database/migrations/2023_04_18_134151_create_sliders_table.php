299dfe31de5c11526d9bfb94a4ad5469e0b87356
mobinajafarian <nimobina99@gmail.com>
2023-04-18 17:42:09 +0330

    add sliders Table

---
+++ database/migrations/2023_04_18_134151_create_sliders_table.php
@@ -0,0 +1,30 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::create('sliders', function (Blueprint $table) {
+            $table->id();
+            $table->string('title')->nullable();
+            $table->string('url',1024)->nullable();
+            $table->string('string',1024)->nullable();
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::dropIfExists('sliders');
+    }
+};
