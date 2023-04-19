e09985efadffb15a9ff18fb4caa066f03202f152
mobinajafarian <nimobina99@gmail.com>
2023-04-19 14:51:18 +0330

    add saveImage

---
+++ app/Models/Brand.php
@@ -0,0 +1,34 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
+use Intervention\Image\Facades\Image;
+use Illuminate\Support\Facades\Storage;
+
+class Brand extends Model
+{
+    use HasFactory;
+
+    protected $fillable = [
+        'title',
+        'image'
+    ];
+
+    public static function saveImage($file){
+        if($file){
+            $name = time().'.'.$file->extension();
+            $smallImage = Image::make($file->getRealPath());
+            $bigImage = Image::make($file->getRealPath());
+            $smallImage->resize(256,256,function ($constraint){
+                $constraint->aspectRatio();
+            });
+            Storage::disk('local')->put('admin/brands/small/'.$name,(string) $smallImage->encode('png',90));
+            Storage::disk('local')->put('admin/brands/big/'.$name,(string) $bigImage->encode('png',90));
+            return $name;
+        }else{
+            return '';
+        }
+    }
+}
