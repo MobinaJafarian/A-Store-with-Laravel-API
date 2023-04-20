71919018eed602bb47311b31612997083135b6db
mobinajafarian <nimobina99@gmail.com>
2023-04-20 16:38:25 +0330

    complate productController

--- app/Http/Controllers/Admin/ProductController.php
+++ app/Http/Controllers/Admin/ProductController.php
@@ -2,10 +2,13 @@
 
 namespace App\Http\Controllers\Admin;
 
+use App\Helpers\Helper;
 use App\Http\Controllers\Controller;
 use App\Models\Brand;
 use App\Models\Category;
 use App\Models\Color;
+use App\Models\Product;
+use Hekmatinasser\Verta\Verta;
 use Illuminate\Http\Request;
 
 class ProductController extends Controller
@@ -36,7 +39,27 @@ class ProductController extends Controller
      */
     public function store(Request $request)
     {
-        
+        $image = Product::saveImage($request->file);
+          $product = Product::query()->create([
+            'title'=>$request->input('title'),
+            'title_en'=>$request->input('title_en'),
+            'slug'=> Helper::make_slug($request->input('title')),
+            'price'=>$request->input('price'),
+            'count'=>$request->input('count'),
+            'image'=>$image,
+            'guaranty'=>$request->input('guaranty'),
+            'discount'=>$request->input('discount'),
+            'description'=>$request->input('description'),
+            'is_special'=>$request->input('is_special') === 'on',
+            'special_expiration'=>($request->input('special_expiration') !==null  ? Verta::parse($request->input('special_expiration'))->datetime() : now()),
+            'category_id'=>$request->input('category_id'),
+            'brand_id'=>$request->input('brand_id'),
+        ]);
+
+          $colors = $request->input('colors');
+          $product->colors()->attach($colors);
+
+        return redirect()->route('products.index')->with('message', 'محصول با موفقیت اضافه شد');
     }
 
     /**
@@ -53,7 +76,11 @@ class ProductController extends Controller
     public function edit(string $id)
     {
         $title = "ویرایش محصول";
-        return view('admin.product.edit', compact('title'));
+        $product = Product::query()->find($id);
+        $categories = Category::query()->pluck('title', 'id');
+        $brands = Brand::query()->pluck('title', 'id');
+        $colors = Color::query()->pluck('title', 'id');
+        return view('admin.product.edit', compact('title','product','categories','brands','colors'));
     }
 
     /**
@@ -61,7 +88,28 @@ class ProductController extends Controller
      */
     public function update(Request $request, string $id)
     {
-        //
+        $product = Product::query()->find($id);
+        $image = Product::saveImage($request->file);
+        $product->update([
+            'title'=>$request->input('title'),
+            'title_en'=>$request->input('title_en'),
+            'slug'=> Helper::make_slug($request->input('title')),
+            'price'=>$request->input('price'),
+            'count'=>$request->input('count'),
+            'image'=>($request->file ? $image : $product->image),
+            'guaranty'=>$request->input('guaranty'),
+            'discount'=>$request->input('discount'),
+            'description'=>$request->input('description'),
+            'is_special'=>$request->input('is_special') === 'on',
+            'special_expiration'=>($request->input('special_expiration') !==null  ? Verta::parse($request->input('special_expiration'))->datetime() : now()),
+            'category_id'=>$request->input('category_id'),
+            'brand_id'=>$request->input('brand_id'),
+        ]);
+
+        $colors = $request->input('colors');
+        $product->colors()->sync($colors);
+
+        return redirect()->route('products.index')->with('message', 'محصول با موفقیت ویرایش شد');
     }
 
     /**
