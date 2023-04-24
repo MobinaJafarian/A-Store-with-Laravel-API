15cb7778deb88893832852dc16e61afc43e068c2
mobinajafarian <nimobina99@gmail.com>
2023-04-24 10:16:51 +0330

    update route name

--- resources/views/admin/product/create_gallery.blade.php
+++ resources/views/admin/product/create_gallery.blade.php
@@ -6,7 +6,7 @@
             <div class="card-body">
                 <div class="container">
                     <h6 class="card-title">  ایجاد لیست تصاویر محصول {{$product->title}}</h6>
-                    <form method="POST" class="dropzone border border-primary" action="{{route('store.product.galley', $product->id)}}"  >
+                    <form method="POST" class="dropzone border border-primary" action="{{route('store.product.gallery', $product->id)}}">
                         @csrf
                         <div class="form-group row">
                             <div class="fallback">
