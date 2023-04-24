b263ef7e895b7db2112941d339e2268db1c1f4e7
mobinajafarian <nimobina99@gmail.com>
2023-04-24 10:15:03 +0330

    update gallery controller

--- app/Http/Controllers/Admin/GalleryController.php
+++ app/Http/Controllers/Admin/GalleryController.php
@@ -12,15 +12,15 @@ class GalleryController extends Controller
     public function addGallery($id)
     {
         $product = Product::query()->find($id);
-        return view('admin.product.create_gallery', compact('product'));
-    } 
-    
-    public function storeGallery(Request $request , $id)
+        return view('admin.product.create_gallery', compact('product','id'));
+    }
+
+    public function storeGallery(Request $request, $id)
     {
         $image = Gallery::saveImage($request->file);
         Gallery::query()->create([
-            'image'=>'',
-            'product_id'=>$id
+            'image' => $image,
+            'product_id' => $id,
         ]);
 
         return redirect()->back()->with('message', 'عکس با موفقیت ذخیره شد');
