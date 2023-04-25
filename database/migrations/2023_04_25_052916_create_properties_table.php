1ad34aa895471cf425b7b4838347b850f7ea11bf
mobinajafarian <nimobina99@gmail.com>
2023-04-25 09:47:04 +0330

    add Properties table

---
+++ database/migrations/2023_04_25_052916_create_properties_table.php
@@ -0,0 +1,30 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::create('properties', function (Blueprint $table) {
+            $table->id();
+            $table->string('title')->nullable();
+            $table->unsignedBigInteger('property_group_id');
+            $table->foreign('property_group_id')->references('id')->on('property_groups')->cascadeOnUpdate()->cascadeOnDelete();
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::dropIfExists('properties');
+    }
+};
