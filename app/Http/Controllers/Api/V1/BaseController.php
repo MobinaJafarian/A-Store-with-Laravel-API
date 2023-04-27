5f949724d617a4d8c4d5e9588a402a13c43e4170
mobinajafarian <nimobina99@gmail.com>
2023-04-27 14:52:21 +0330

    add baseController

---
+++ app/Http/Controllers/Api/V1/BaseController.php
@@ -0,0 +1,18 @@
+<?php
+
+namespace App\Http\Controllers\Api\V1;
+
+use App\Http\Controllers\Controller;
+use Illuminate\Http\Request;
+
+/**
+ * @OA\Info(
+ *     version="1.0",
+ *     title="API"
+ * )
+ *  @OA\PathItem(path="/api")
+ */
+class BaseController extends Controller
+{
+    //
+}
