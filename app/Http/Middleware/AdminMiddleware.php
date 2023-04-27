956ad7666c9d4dc0b4707d107c9bdb4b13287184
mobinajafarian <nimobina99@gmail.com>
2023-04-27 11:21:05 +0330

    add adminMiddleware

---
+++ app/Http/Middleware/AdminMiddleware.php
@@ -0,0 +1,26 @@
+<?php
+
+namespace App\Http\Middleware;
+
+use Closure;
+use Illuminate\Http\Request;
+use Symfony\Component\HttpFoundation\Response;
+
+class AdminMiddleware
+{
+    /**
+     * Handle an incoming request.
+     *
+     * @param  \Closure(\Illuminate\Http\Request): (\Symfony\Component\HttpFoundation\Response)  $next
+     */
+    public function handle(Request $request, Closure $next): Response
+    {
+        $user = auth()->user();
+        if($user->is_admin || $user->hasRole(['Super Admin'])){
+            return $next($request);
+        }else{
+            // return redirect()->route('forbidden');
+            return abort('403'); 
+        }
+    }
+}
