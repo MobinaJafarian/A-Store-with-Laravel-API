eee9da42724e51f6278caabdacfa45f4e04f6070
mobinajafarian <nimobina99@gmail.com>
2023-04-28 23:25:06 +0330

    add checkTwoMinute

--- app/Models/SmsCode.php
+++ app/Models/SmsCode.php
@@ -2,6 +2,7 @@
 
 namespace App\Models;
 
+use Carbon\Carbon;
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
 
@@ -13,4 +14,14 @@ class SmsCode extends Model
         'mobile',
         'code'
     ];
+
+    public static function checkTwoMinute($mobile){
+        $check = self::query()->where('mobile', $mobile)->where('created_at','>',Carbon::now()->subMinute(2))->first();
+
+        if($check){
+            return true;
+        }else{
+            return false;
+        }
+    }
 }
