cb5f56de98bd5a1313edf8ed715b3f64af14f093
mobinajafarian <nimobina99@gmail.com>
2023-04-28 23:07:38 +0330

    add api routes

--- routes/api.php
+++ routes/api.php
@@ -1,5 +1,6 @@
 <?php
 
+use App\Http\Controllers\Api\V1\AuthController;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
 
@@ -16,4 +17,9 @@ use Illuminate\Support\Facades\Route;
 
 Route::middleware('auth:sanctum')->get('/user', function (Request $request) {
     return $request->user();
+});
+
+Route::prefix('/v1')->namespace('Api\V1')->group(function(){
+    Route::post('send_sms', [AuthController::class, 'sendSms']);
+
 });
