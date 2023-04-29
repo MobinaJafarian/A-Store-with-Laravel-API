1509d098d3a61263cac32979c438a4eedc89101e
mobinajafarian <nimobina99@gmail.com>
2023-04-29 08:21:54 +0330

    add verifySms

--- app/Http/Controllers/Api/V1/AuthController.php
+++ app/Http/Controllers/Api/V1/AuthController.php
@@ -4,8 +4,10 @@ namespace App\Http\Controllers\Api\V1;
 
 use App\Http\Controllers\Controller;
 use App\Models\SmsCode;
+use App\Models\User;
 use GuzzleHttp\Psr7\Response;
 use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Hash;
 
 class AuthController extends Controller
 {
@@ -58,6 +60,76 @@ class AuthController extends Controller
                 'message'=>"please wait for two minutes",
                 'data'=>[]
             ],403);
+        }
+    }
+
+    /**
+     * @OA\Post(
+     ** path="/api/v1/verify_sms_code",
+     *  tags={"Auth Api"},
+     *  description="use to check sms code that recieved by user",
+     * @OA\RequestBody(
+     *    required=true,
+     * *         @OA\MediaType(
+     *           mediaType="multipart/form-data",
+     *           @OA\Schema(
+     *           @OA\Property(
+     *                  property="mobile",
+     *                  description="user mobile number",
+     *                  type="string",
+     *               ),
+     *           @OA\Property(
+     *                  property="code",
+     *                  description="recieved user sms code",
+     *                  type="string",
+     *               ),
+     *     )
+     *   )
+     * ),
+     *   @OA\Response(
+     *      response=200,
+     *      description="Its Ok",
+     *      @OA\MediaType(
+     *           mediaType="application/json",
+     *      )
+     *   )
+     *)
+     **/
+    public function verifySms(Request $request)
+    {
+        $mobile = $request->input('mobile');
+        $code = $request->input('code');
+
+        $check = SmsCode::checkSend($mobile, $code);
+        if($check){
+
+            $user = User::query()->where('mobile', $mobile)->first();
+
+            if($user){
+                return Response()->json([
+                    'result'=>true,
+                    'message'=>"user registered before",
+                    'data'=>[
+                        'id'=>$user->id,
+                        'token'=>$user->createToken("new Token")->plainTextToken
+                    ]
+                ],201);
+            }else{
+
+                $user = User::query()->create([
+                    'mobile'=>$mobile,
+                    'password'=> Hash::make(rand(1111,9999))
+                ]);
+
+                return Response()->json([
+                    'result'=>true,
+                    'message'=>"new user created",
+                    'data'=>[
+                        'id'=>$user->id,
+                        'token'=>$user->createToken("new Token")->plainTextToken
+                    ]
+                ],201);
+            }
         }
     }
 }
