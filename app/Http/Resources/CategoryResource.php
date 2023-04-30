b7cf7161f86deb9bc640ad4fe40dc3b78c31657c
mobinajafarian <nimobina99@gmail.com>
2023-04-30 09:44:05 +0330

    add CategoryResource

---
+++ app/Http/Resources/CategoryResource.php
@@ -0,0 +1,24 @@
+<?php
+
+namespace App\Http\Resources;
+
+use Illuminate\Http\Request;
+use Illuminate\Http\Resources\Json\JsonResource;
+
+class CategoryResource extends JsonResource
+{
+    /**
+     * Transform the resource into an array.
+     *
+     * @return array<string, mixed>
+     */
+    public function toArray(Request $request): array
+    {
+        return [
+            'id' => $this->id,
+            'title' => $this->title,
+            'image' => url('images/admin/categories/big/' . $this->image),
+            'parent_id' => $this->parent_id,
+        ];
+    }
+}
