64e5cb03dc9deacb67052f3e50010fce1990fb72
mobinajafarian <nimobina99@gmail.com>
2023-04-30 09:37:46 +0330

    add HomeApiController

---
+++ app/Http/Controllers/Api/V1/HomeApiController.php
@@ -0,0 +1,28 @@
+<?php
+
+namespace App\Http\Controllers\Api\V1;
+
+use App\Http\Controllers\Controller;
+use App\Http\services\Keys;
+use App\Models\Category;
+use App\Models\Slider;
+
+class HomeApiController extends Controller
+{
+    public function home()
+    {
+        return response()->json([
+            'result' => true,
+            'message' => 'application home page',
+            'data' => [
+                Keys::sliders => Slider::getSliders(),
+                Keys::categories => Category::getAllCategories(),
+                Keys::amazing_products => ProductRepository::get6AmazingProducts(),
+                Keys::banner => Slider::query()->inRandomOrder()->first(),
+                Keys::most_seller_products => ProductRepository::get6MostSellerProducts(),
+                Keys::newest_products => ProductRepository::get6NewestProducts(),
+            ],
+
+        ], 200);
+    }
+}
