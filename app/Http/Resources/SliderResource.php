77c22250452f2b260374c4afa7eea8a5e7ba4049
mobinajafarian <nimobina99@gmail.com>
2023-04-30 09:39:12 +0330

    add sliderResource

---
+++ app/Http/Resources/SliderResource.php
@@ -0,0 +1,23 @@
+<?php
+
+namespace App\Http\Resources;
+
+use Illuminate\Http\Request;
+use Illuminate\Http\Resources\Json\JsonResource;
+
+class SliderResource extends JsonResource
+{
+    /**
+     * Transform the resource into an array.
+     *
+     * @return array<string, mixed>
+     */
+    public function toArray(Request $request): array
+    {
+        return [
+            'id' => $this->id,
+            'title' => $this->title,
+            'image' => url('images/admin/sliders/big/' . $this->image),
+        ];
+    }
+}
