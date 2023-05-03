35a77e650952894d472798887d115229bb74e295
mobinajafarian <nimobina99@gmail.com>
2023-05-03 10:22:27 +0330

    add ProductRepository

--- app/Http/Controllers/Api/V1/HomeApiController.php
+++ app/Http/Controllers/Api/V1/HomeApiController.php
@@ -3,6 +3,7 @@
 namespace App\Http\Controllers\Api\V1;
 
 use App\Http\Controllers\Controller;
+use App\Http\Repositories\ProductRepository;
 use App\Http\services\Keys;
 use App\Models\Category;
 use App\Models\Slider;
