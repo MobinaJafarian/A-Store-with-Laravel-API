6bc8603ecdad67c68077643b618cf75fbe0b0611
mobinajafarian <nimobina99@gmail.com>
2023-05-03 12:48:48 +0330

    add OA f

--- app/Http/Controllers/Api/V1/HomeApiController.php
+++ app/Http/Controllers/Api/V1/HomeApiController.php
@@ -4,12 +4,26 @@ namespace App\Http\Controllers\Api\V1;
 
 use App\Http\Controllers\Controller;
 use App\Http\Repositories\ProductRepository;
-use App\Http\services\Keys;
+use App\Providers\KeysServiceProvider as keys;
 use App\Models\Category;
 use App\Models\Slider;
 
 class HomeApiController extends Controller
 {
+    /**
+     * @OA\Get(
+     ** path="/api/v1/home",
+     *  tags={"Home Page"},
+     *  description="get home page data",
+     *   @OA\Response(
+     *      response=200,
+     *      description="Its Ok",
+     *      @OA\MediaType(
+     *           mediaType="application/json",
+     *      )
+     *   )
+     *)
+     **/
     public function home()
     {
         return response()->json([
