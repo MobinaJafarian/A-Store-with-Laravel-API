c4b7d88b41c408c422f28a0e30f4ef6e13304629
mobinajafarian <nimobina99@gmail.com>
2023-05-04 23:40:11 +0330

    rename key most_visited_product

--- app/Providers/KeysServiceProvider.php
+++ app/Providers/KeysServiceProvider.php
@@ -14,7 +14,7 @@ class KeysServiceProvider extends ServiceProvider
     const amazing_products = 'amazing_products';
     const banner = 'banner';
     const most_seller_products = 'most_seller_products';
-    const most_viewed_products = 'most_viewed_products';
+    const most_visited_products = 'most_visited_products';
     const cheapest_products = 'cheapest_products';
     const most_expensive_products = 'most_expensive_products';
     const newest_products = 'newest_products';
