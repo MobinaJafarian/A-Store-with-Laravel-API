170ba218e6e8dd8b0411015b5796bec7571e5b03
mobinajafarian <nimobina99@gmail.com>
2023-05-05 00:20:05 +0330

    add OA swagger

--- app/Http/Controllers/Api/V1/ProductApiController.php
+++ app/Http/Controllers/Api/V1/ProductApiController.php
@@ -4,72 +4,214 @@ namespace App\Http\Controllers\Api\V1;
 
 use App\Http\Controllers\Controller;
 use App\Http\Repositories\ProductRepository;
+use App\Models\Brand;
 use App\Models\Category;
 use App\Providers\KeysServiceProvider as keys;
 use Illuminate\Http\Request;
 
 class ProductApiController extends Controller
 {
+        /**
+     * @OA\Get(
+     ** path="/api/v1/most_sold_products",
+     *  tags={"Products Page"},
+     *  description="get products page data",
+     *   @OA\Response(
+     *      response=200,
+     *      description="Its Ok",
+     *      @OA\MediaType(
+     *           mediaType="application/json",
+     *      )
+     *   )
+     *)
+     **/
     public function most_sold_products()
     {
         return response()->json([
             'result' => true,
             'message' => 'application home page',
             'data' => [
-                Keys::categories => Category::getAllCategories(),
+                Keys::brands => Brand::getAllBrands(),
                 Keys::most_seller_products => ProductRepository::getMostSellerProducts()->response()->getData(true),
             ],
 
         ], 200);
     } 
 
+        /**
+     * @OA\Get(
+     ** path="/api/v1/most_visited_products",
+     *  tags={"Products Page"},
+     *  description="get products page data",
+     *   @OA\Response(
+     *      response=200,
+     *      description="Its Ok",
+     *      @OA\MediaType(
+     *           mediaType="application/json",
+     *      )
+     *   )
+     *)
+     **/
     public function most_visited_products()
     {
         return response()->json([
             'result' => true,
             'message' => 'application home page',
             'data' => [
-                Keys::categories => Category::getAllCategories(),
+                Keys::brands => Brand::getAllBrands(),
                 Keys::most_visited_products => ProductRepository::getMostVisitedProducts()->response()->getData(true),
             ],
 
         ], 200);
     } 
 
+        /**
+     * @OA\Get(
+     ** path="/api/v1/newest_products",
+     *  tags={"Products Page"},
+     *  description="get products page data",
+     *   @OA\Response(
+     *      response=200,
+     *      description="Its Ok",
+     *      @OA\MediaType(
+     *           mediaType="application/json",
+     *      )
+     *   )
+     *)
+     **/
     public function newest_products()
     {
         return response()->json([
             'result' => true,
             'message' => 'application home page',
             'data' => [
-                Keys::categories => Category::getAllCategories(),
+                Keys::brands => Brand::getAllBrands(),
                 Keys::newest_products => ProductRepository::getNewestProducts()->response()->getData(true),
             ],
 
         ], 200);
     } 
 
+        /**
+     * @OA\Get(
+     ** path="/api/v1/cheapest_products",
+     *  tags={"Products Page"},
+     *  description="get products page data",
+     *   @OA\Response(
+     *      response=200,
+     *      description="Its Ok",
+     *      @OA\MediaType(
+     *           mediaType="application/json",
+     *      )
+     *   )
+     *)
+     **/
     public function cheapets_products()
     {
         return response()->json([
             'result' => true,
             'message' => 'application home page',
             'data' => [
-                Keys::categories => Category::getAllCategories(),
+                Keys::brands => Brand::getAllBrands(),
                 Keys::cheapest_products => ProductRepository::getCheapetsProducts()->response()->getData(true),
             ],
 
         ], 200);
     } 
 
+
+    /**
+     * @OA\Get(
+     ** path="/api/v1/most_expensive_products",
+     *  tags={"Products Page"},
+     *  description="get products page data",
+     *   @OA\Response(
+     *      response=200,
+     *      description="Its Ok",
+     *      @OA\MediaType(
+     *           mediaType="application/json",
+     *      )
+     *   )
+     *)
+     **/
     public function most_expensive_products()
     {
         return response()->json([
             'result' => true,
             'message' => 'application home page',
             'data' => [
-                Keys::categories => Category::getAllCategories(),
+                Keys::brands => Brand::getAllBrands(),
                 Keys::most_expensive_products => ProductRepository::getMostExpensivesProducts()->response()->getData(true),
+            ],
+
+        ], 200);
+    } 
+    
+        /**
+     * @OA\Get(
+     ** path="/api/v1/products_by_category/{id}",
+     *  tags={"Products Page"},
+     *  description="get products data by category id",
+     * *     @OA\Parameter(
+     *         in="path",
+     *         name="id",
+     *         required=true,
+     *         @OA\Schema(
+     *             type="integer",
+     *         ),
+     *     ),
+     *   @OA\Response(
+     *      response=200,
+     *      description="Its Ok",
+     *      @OA\MediaType(
+     *           mediaType="application/json",
+     *      )
+     *   )
+     *)
+     **/
+    public function productsByCategory($id)
+    {
+        return response()->json([
+            'result' => true,
+            'message' => 'application products page',
+            'data' => [
+                Keys::categories => Category::getAllCategories(),
+                Keys::products_by_category => ProductRepository::getProductsByCategory($id)->response()->getData(true),
+            ],
+
+        ], 200);
+    } 
+    
+       /**
+     * @OA\Get(
+     ** path="/api/v1/products_by_brand/{id}",
+     *  tags={"Products Page"},
+     *  description="get products data by category id",
+     * *     @OA\Parameter(
+     *         in="path",
+     *         name="id",
+     *         required=true,
+     *         @OA\Schema(
+     *             type="integer",
+     *         ),
+     *     ),
+     *   @OA\Response(
+     *      response=200,
+     *      description="Its Ok",
+     *      @OA\MediaType(
+     *           mediaType="application/json",
+     *      )
+     *   )
+     *)
+     **/
+    public function productsByBrand($id)
+    {
+        return response()->json([
+            'result' => true,
+            'message' => 'application products page',
+            'data' => [
+                Keys::categories => Category::getAllCategories(),
+                Keys::products_by_brand => ProductRepository::getProductsByBrand($id)->response()->getData(true),
             ],
 
         ], 200);
