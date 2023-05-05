35bd8537a9f8fe756dc16f0e1dad71a9593e4b20
mobinajafarian <nimobina99@gmail.com>
2023-05-05 09:54:12 +0330

    add resources

---
+++ app/Http/Resources/CommentResource.php
@@ -0,0 +1,22 @@
+<?php
+
+namespace App\Http\Resources;
+
+use Illuminate\Http\Request;
+use Illuminate\Http\Resources\Json\JsonResource;
+
+class CommentResource extends JsonResource
+{
+    /**
+     * Transform the resource into an array.
+     *
+     * @return array<string, mixed>
+     */
+    public function toArray(Request $request): array
+    {
+        return [
+            'body' => $this->body,
+            'user' => $this->user->name,
+        ];
+    }
+}
---
+++ app/Http/Resources/PropertyResource.php
@@ -0,0 +1,22 @@
+<?php
+
+namespace App\Http\Resources;
+
+use Illuminate\Http\Request;
+use Illuminate\Http\Resources\Json\JsonResource;
+
+class PropertyResource extends JsonResource
+{
+    /**
+     * Transform the resource into an array.
+     *
+     * @return array<string, mixed>
+     */
+    public function toArray(Request $request): array
+    {
+        return [
+            'title' => $this->title,
+            'property_group' => $this->property_group->title,
+        ];
+    }
+}
