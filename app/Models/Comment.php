27cc03264448bbf3893b7fedab7ff19a5940a2fd
mobinajafarian <nimobina99@gmail.com>
2023-05-05 09:54:53 +0330

    add model comment

---
+++ app/Models/Comment.php
@@ -0,0 +1,29 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
+
+class Comment extends Model
+{
+    use HasFactory;
+
+    protected $fillable =[
+        'body',
+        'status',
+        'parent_id',
+        'product_id',
+        'user_id'
+    ];
+
+    public function user()
+    {
+        return $this->belongsTo(User::class);
+    }
+
+    public function product()
+    {
+        return $this->belongsTo(Product::class);
+    }
+}
