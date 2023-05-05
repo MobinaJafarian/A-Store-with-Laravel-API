92fc132e08bff25254428015dea94901091d7dd0
mobinajafarian <nimobina99@gmail.com>
2023-05-05 14:25:35 +0330

    add searchProduct

--- app/Http/Controllers/Api/V1/ProductApiController.php
+++ app/Http/Controllers/Api/V1/ProductApiController.php
@@ -319,4 +319,43 @@ class ProductApiController extends Controller
 
         ],200);
     }
+
+        /**
+     * @OA\Post(
+     ** path="/api/v1/search_product",
+     *  tags={"Products Page"},
+     *  description="search product",
+     *    @OA\RequestBody(
+     *    required=true,
+     *          @OA\MediaType(
+     *           mediaType="multipart/form-data",
+     *           @OA\Schema(
+     *           @OA\Property(
+     *                  property="search",
+     *                  type="string",
+     *               ),
+     *     )
+     *   )
+     * ),
+     *   @OA\Response(
+     *      response=200,
+     *      description="Its Ok",
+     *      @OA\MediaType(
+     *           mediaType="application/json",
+     *      )
+     *   )
+     *)
+     **/
+    public function searchProduct(Request $request)
+    {
+        return response()->json([
+            'result'=>true,
+            'message'=>'application products page',
+            'data'=>[
+                Keys::brands=>Brand::getAllBrands(),
+                Keys::searched_products=>ProductRepository::searchedProduct($request->input('search'))->response()->getData(true),
+            ]
+
+        ],200);
+    }
 }
