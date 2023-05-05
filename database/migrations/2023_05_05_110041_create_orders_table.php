b7699527c1ac5aec6db542ef46824fa0295351cc
mobinajafarian <nimobina99@gmail.com>
2023-05-05 14:51:01 +0330

    add orders table & model

---
+++ database/migrations/2023_05_05_110041_create_orders_table.php
@@ -0,0 +1,36 @@
+<?php
+
+use App\Enums\PaymentStatus;
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::create('orders', function (Blueprint $table) {
+            $table->id();
+            $table->integer('total_price')->default(0);
+            $table->integer('code')->default(0);
+            $table->string('status')->default(PaymentStatus::Draft->value);
+            $table->string('transaction_id')->nullable();
+            $table->unsignedBigInteger('address_id');
+            $table->foreign('address_id')->references('id')->on('addresses');
+            $table->unsignedBigInteger('user_id');
+            $table->foreign('user_id')->references('id')->on('users');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::dropIfExists('orders');
+    }
+};
