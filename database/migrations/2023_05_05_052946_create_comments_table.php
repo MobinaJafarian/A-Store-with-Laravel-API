27748fd0894e2e237a5e83a81d3b128f09e4750c
mobinajafarian <nimobina99@gmail.com>
2023-05-05 09:53:39 +0330

    create Comments table

---
+++ database/migrations/2023_05_05_052946_create_comments_table.php
@@ -0,0 +1,35 @@
+<?php
+
+use App\Enums\CommentStatus;
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::create('comments', function (Blueprint $table) {
+            $table->id();
+            $table->string('body')->nullable();
+            $table->string('status')->default(CommentStatus::Draft->value);
+            $table->unsignedBigInteger('parent_id')->nullable();
+            $table->unsignedBigInteger('user_id');
+            $table->foreign('user_id')->references('id')->on('users');
+            $table->unsignedBigInteger('product_id');
+            $table->foreign('product_id')->references('id')->on('products');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::dropIfExists('comments');
+    }
+};
