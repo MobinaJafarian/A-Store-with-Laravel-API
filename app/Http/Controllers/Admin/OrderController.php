5f8acaa7e9af98f8efe73ebd275fc8973f7bc658
mobinajafarian <nimobina99@gmail.com>
2023-05-07 15:54:21 +0330

    add  orderDetails

--- app/Http/Controllers/Admin/OrderController.php
+++ app/Http/Controllers/Admin/OrderController.php
@@ -9,6 +9,11 @@ class OrderController extends Controller
 {
     public function orders()
     {
-        return view('admin.order.list');
+        return view('admin.order.order');
+    }
+    
+    public function orderDetails($order_id)
+    {
+        return view('admin.order.order_details', compact('order_id'));
     }
 }
