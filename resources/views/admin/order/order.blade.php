69f615db26d81ff0f63ca3e860e8dcc6c4b65e6e
mobinajafarian <nimobina99@gmail.com>
2023-05-07 15:53:36 +0330

    update orders blade

---
+++ resources/views/admin/order/order.blade.php
@@ -0,0 +1,17 @@
+@extends('admin.layouts.master')
+@section('content')
+    <main class="main-content">
+        <div class="row">
+            @if(\Illuminate\Support\Facades\Session::has('message'))
+                <div class="alert alert-info">
+                    <div>{{session('message')}}</div>
+                </div>
+            @endif
+        </div>
+        <div class="card">
+            <div class="card-body">
+              <livewire:admin.orders/>
+            </div>
+        </div>
+    </main>
+@endsection
