a3c23b3425dd59869c1a83083b1476073cf462df
mobinajafarian <nimobina99@gmail.com>
2023-05-07 14:50:10 +0330

    add orders routes

--- routes/web.php
+++ routes/web.php
@@ -5,6 +5,7 @@ use App\Http\Controllers\Admin\CategoryController;
 use App\Http\Controllers\Admin\ColorController;
 use App\Http\Controllers\Admin\GalleryController;
 use App\Http\Controllers\Admin\LogViewerController;
+use App\Http\Controllers\Admin\OrderController;
 use App\Http\Controllers\Admin\PanelController;
 use App\Http\Controllers\Admin\ProductController;
 use App\Http\Controllers\Admin\PropertyController;
@@ -25,7 +26,7 @@ use Illuminate\Support\Facades\Route;
 | routes are loaded by the RouteServiceProvider and all of them will
 | be assigned to the "web" middleware group. Make something great!
 |
-*/
+ */
 
 Route::get('/', function () {
     return redirect()->route('login');
@@ -41,18 +42,17 @@ Route::middleware('auth')->group(function () {
     Route::delete('/profile', [ProfileController::class, 'destroy'])->name('profile.destroy');
 });
 
+Route::prefix('admin')->middleware(['auth', 'admin'])->group(function () {
 
-Route::prefix('admin')->middleware(['auth','admin'])->group(function () {
-    
     //Main Route
-    Route::get('/', [PanelController::class , 'index'])->name('panel');
-    
+    Route::get('/', [PanelController::class, 'index'])->name('panel');
+
     //User Route
     Route::resource('/users', UserController::class);
     Route::resource('/roles', RoleController::class);
-    Route::get('create_user_roles/{id}', [UserController::class , 'createUserRoles'])->name('create.user.roles');
-    Route::post('store_user_roles/{id}', [UserController::class , 'storeUserRoles'])->name('store.user.roles');
-    Route::get('logs', [LogViewerController::class , 'index'])->name('logs');
+    Route::get('create_user_roles/{id}', [UserController::class, 'createUserRoles'])->name('create.user.roles');
+    Route::post('store_user_roles/{id}', [UserController::class, 'storeUserRoles'])->name('store.user.roles');
+    Route::get('logs', [LogViewerController::class, 'index'])->name('logs');
 
     //Product Route
     Route::resource('category', CategoryController::class);
@@ -63,12 +63,13 @@ Route::prefix('admin')->middleware(['auth','admin'])->group(function () {
     Route::resource('property_groups', PropertyGroupController::class);
     Route::resource('properties', PropertyController::class);
 
-    Route::get('create_product_gallery/{id}',[GalleryController::class, 'addGallery'])->name('create.product.gallery');
-    Route::post('store_product_gallery/{id}',[GalleryController::class, 'storeGallery'])->name('store.product.gallery');
-    
-    Route::get('create_product_properties/{id}',[ProductController::class, 'addProperties'])->name('create.product.properties');
-    Route::post('store_product_properties/{id}',[ProductController::class, 'storeProperties'])->name('store.product.properties');
+    Route::get('create_product_gallery/{id}', [GalleryController::class, 'addGallery'])->name('create.product.gallery');
+    Route::post('store_product_gallery/{id}', [GalleryController::class, 'storeGallery'])->name('store.product.gallery');
+
+    Route::get('create_product_properties/{id}', [ProductController::class, 'addProperties'])->name('create.product.properties');
+    Route::post('store_product_properties/{id}', [ProductController::class, 'storeProperties'])->name('store.product.properties');
+
+    Route::get('orders', [OrderController::class, 'orders'])->name('orders.panel');
 });
 
-
-require __DIR__.'/auth.php';
+require __DIR__ . '/auth.php';
