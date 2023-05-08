4eaa04afee963dd81a91936f2ac16361ce175dee
mobinajafarian <nimobina99@gmail.com>
2023-05-08 10:04:41 +0330

    add item counts to admin dashboard

--- app/Http/Controllers/Admin/PanelController.php
+++ app/Http/Controllers/Admin/PanelController.php
@@ -3,12 +3,21 @@
 namespace App\Http\Controllers\Admin;
 
 use App\Http\Controllers\Controller;
+use App\Models\Comment;
+use App\Models\Order;
+use App\Models\Product;
+use App\Models\User;
 use Illuminate\Http\Request;
+use Illuminate\Support\Facades\DB;
 
 class PanelController extends Controller
 {
     public function index()
     {
-        return view('admin.index');
+        $users = User::all()->count();
+        $products = Product::all()->count();
+        $orders = Order::all()->count();
+        $comments = Comment::all()->count();
+        return view('admin.index', compact('users', 'products', 'orders', 'comments'));
     }
 }
