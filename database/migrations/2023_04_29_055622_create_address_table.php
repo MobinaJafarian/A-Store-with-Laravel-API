a2fb0f0aadb85582ae96d5a16f98de12e1d9e6ad
Mobina Jafarian <mobina.nezhadjfarian@gmail.com>
2023-11-29 11:31:14 +0330

    fix addresses table name

--- database/migrations/2023_04_29_055622_create_address_table.php
+++ database/migrations/2023_04_29_055622_create_address_table.php
@@ -11,7 +11,7 @@ return new class extends Migration
      */
     public function up(): void
     {
-        Schema::create('address', function (Blueprint $table) {
+        Schema::create('addresses', function (Blueprint $table) {
             $table->id();
             $table->string('address', 255)->nullable();
             $table->string('postal_code')->nullable();
